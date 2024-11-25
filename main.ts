43a8cf3a177ca48ceb97f1760312050b42dd8e76
digidagsOrg <107912351+digidagsOrg@users.noreply.github.com>
2024-11-25 16:39:29 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,6 @@ let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
 strip.setBrightness(50)
 strip.showRainbow(1, 360)
 basic.forever(function () {
-    for (let index = 0; index <= 700; index++) {
-        pins.analogWritePin(AnalogPin.P1, index)
-        basic.pause(100)
-    }
-    for (let index = 0; index <= 700; index++) {
-        pins.analogWritePin(AnalogPin.P1, 1023 - index)
-        basic.pause(100)
-    }
-})
-basic.forever(function () {
     strip.rotate(1)
     strip.show()
     basic.pause(100)
@@ -24,6 +14,16 @@ basic.forever(function () {
     }
     for (let index = 0; index <= 700; index++) {
         pins.analogWritePin(AnalogPin.P2, 1023 - index)
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
+    for (let index = 0; index <= 700; index++) {
+        pins.analogWritePin(AnalogPin.P1, index)
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 700; index++) {
+        pins.analogWritePin(AnalogPin.P1, 1023 - index)
         basic.pause(100)
     }
 })
